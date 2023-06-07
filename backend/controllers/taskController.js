57b620170d619f66ffbfe48eb0661791c41ed489
Jesse <jesseorhs@gmail.com>
2023-06-07 16:04:17 -0700

    feat: add createtask fucntionality

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,6 +1,30 @@
+const admin = require('firebase-admin');
+const { StatusCodes } = require('http-status-codes');
+const db = admin.firestore();
+require('http-status-codes')
+
 
 const createTask = async (req, res) => {
-    res.send('create task route')
+    const {title, description, company} = req.body
+    if (!title || !description || !company){
+        return res.status(StatusCodes.BAD_REQUEST).json({error: 'Missing required fields'})
+    }
+    const newTask = {
+        title,
+        description,
+        company,
+        student: null,
+        status: "open",
+        dateCreated: admin.firestore.FieldValue.serverTimestamp(),
+        dateCompleted: null
+    };
+    try {
+         await db.collection('tasks').add(newTask);
+        
+    } catch (error) {
+        console.log(error)
+    }
+    res.status(StatusCodes.CREATED).json({msg: "successfuly created task"})
 }
 
 const getAllTasks = async (req, res) => {
