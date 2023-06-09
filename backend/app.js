d0e1d7d259e65f681832d4bec34e5dff3a43f392
Jesse <jesseorhs@gmail.com>
2023-06-08 17:53:53 -0700

    refactor: adjust code formatting

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,13 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const admin = require('./db/firebaseAdmin');
+const admin = require('./db/firebaseAdmin'); // Removing stops firebase connection
 
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello, world!');
+  res.send('Hello, world!');
 });
 
 const taskRouter = require('./routes/taskRoutes');
@@ -19,5 +19,6 @@ app.use('/dashboard/tasks', taskRouter);
 // Start the server
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  // eslint-disable-next-line no-console
+  console.log(`Server is running on port ${port}`);
 });
--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -5,69 +5,67 @@ const db = admin.firestore();
 require('http-status-codes');
 
 const createTask = async (req, res) => {
-    const { title, description, company } = req.body;
-    if (!title || !description || !company) {
-        return res.status(StatusCodes.BAD_REQUEST).json({
-            error: 'Missing required fields',
-        });
-    }
-    const newTask = {
-        title,
-        description,
-        company,
-        student: null,
-        status: 'open',
-        dateCreated: admin.firestore.FieldValue.serverTimestamp(),
-        dateCompleted: null,
-    };
-    try {
-        await db.collection('tasks').add(newTask);
-    } catch (error) {
-        return res.status(StatusCodes.BAD_REQUEST).json({
-            error: 'Failed to create task',
-        });
-    }
-    res.status(StatusCodes.CREATED).json({ msg: 'Successfuly created task' });
+  const { title, description, company } = req.body;
+  if (!title || !description || !company) {
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      error: 'Missing required fields',
+    });
+  }
+  const newTask = {
+    title,
+    description,
+    company,
+    student: null,
+    status: 'open',
+    dateCreated: admin.firestore.FieldValue.serverTimestamp(),
+    dateCompleted: null,
+  };
+  try {
+    await db.collection('tasks').add(newTask);
+  } catch (error) {
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      error: 'Failed to create task',
+    });
+  }
+  res.status(StatusCodes.CREATED).json({ msg: 'Successfuly created task' });
 };
 
 const getAllTasks = async (req, res) => {
-    const taskCollection = db.collection('tasks');
-    const snapshot = await taskCollection.get();
-    if (snapshot.empty) {
-        return res
-            .status(StatusCodes.NOT_FOUND)
-            .json({ error: 'Tasks not found' });
-    }
-    const tasks = [];
-    snapshot.forEach((doc) => {
-        const { id } = doc;
-        const data = doc.data();
-        tasks.push({ id, ...data });
-    });
-    res.status(StatusCodes.OK).json(tasks);
+  const taskCollection = db.collection('tasks');
+  const snapshot = await taskCollection.get();
+  if (snapshot.empty) {
+    return res.status(StatusCodes.NOT_FOUND).json({ error: 'Tasks not found' });
+  }
+  const tasks = [];
+  snapshot.forEach((doc) => {
+    const { id } = doc;
+    const data = doc.data();
+    tasks.push({ id, ...data });
+  });
+  res.status(StatusCodes.OK).json(tasks);
 };
 
 const getCurrentUserTasks = async (req, res) => {
-    res.send('get current user tasks route');
+  res.send('get current user tasks route');
 };
 
 const updateTask = async (req, res) => {
-    res.send('update task route');
+  res.send('update task route');
 };
 
 const getSingleTask = async (req, res) => {
-    res.send('get single task route');
+  res.send('get single task route');
 };
 
 const deleteTask = async (req, res) => {
-    res.send('delete task route');
+  res.send('delete task route');
 };
 
 module.exports = {
-    createTask,
-    getAllTasks,
-    getCurrentUserTasks,
-    updateTask,
-    getSingleTask,
-    deleteTask,
+  createTask,
+  getAllTasks,
+  getCurrentUserTasks,
+  updateTask,
+  getSingleTask,
+  deleteTask,
 };
