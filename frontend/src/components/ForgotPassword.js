dbb92dd2d19aa3b405b2a6d329691af36e5d91a3
Jesse <jesseorhs@gmail.com>
2023-06-28 16:25:31 -0700

    feat: add nav to forgot password

--- frontend/src/components/ForgotPassword.js
+++ frontend/src/components/ForgotPassword.js
@@ -29,36 +29,43 @@ export default function ForgotPassword() {
   }
 
   return (
-    <div
-      className="d-flex"
-      style={{
-        minHeight: '100vh'
-      }}>
-      <Card
-        className="d-flex align-items-center justify-content-center"
-        style={{ minHeight: '20vh', margin: 'auto', maxWidth: '400px' }}>
-        <Card.Body>
-          <h2 className="text-center mb-4">Password Reset</h2>
-          {message && <Alert variant="success">{message}</Alert>}
-          {error && <Alert variant="danger">{error}</Alert>}
-          <Form onSubmit={handleSubmit}>
-            <Form.Group id="email">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" ref={emailRef} required />
-            </Form.Group>
+    <>
+      <nav className="home-nav">
+        <Link to="/" className="nav-item-left">
+          Tech Incubator
+        </Link>
+      </nav>
+      <div
+        className="d-flex"
+        style={{
+          minHeight: '100vh'
+        }}>
+        <Card
+          className="d-flex align-items-center justify-content-center"
+          style={{ minHeight: '20vh', margin: 'auto', maxWidth: '400px' }}>
+          <Card.Body>
+            <h2 className="text-center mb-4">Password Reset</h2>
+            {message && <Alert variant="success">{message}</Alert>}
+            {error && <Alert variant="danger">{error}</Alert>}
+            <Form onSubmit={handleSubmit}>
+              <Form.Group id="email">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" ref={emailRef} required />
+              </Form.Group>
 
-            <Button disabled={loading} className="w-100 mt-4" type="submit">
-              Reset Password
-            </Button>
-          </Form>
-          <div className="w-100 text-center mt-3">
-            <Link to="/login">Log In</Link>
+              <Button disabled={loading} className="w-100 mt-4" type="submit">
+                Reset Password
+              </Button>
+            </Form>
+            <div className="w-100 text-center mt-3">
+              <Link to="/login">Log In</Link>
+            </div>
+          </Card.Body>
+          <div className="text-center mt-2" style={{ marginBottom: '-32px' }}>
+            Need an account? <Link to="/signup">Sign Up</Link>
           </div>
-        </Card.Body>
-        <div className="text-center mt-2" style={{ marginBottom: '-32px' }}>
-          Need an account? <Link to="/signup">Sign Up</Link>
-        </div>
-      </Card>
-    </div>
+        </Card>
+      </div>
+    </>
   );
 }
